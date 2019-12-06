5710a298ca25c258a31817a79b43e4556b566638
angeloqg <angeloqg@hotmail.com>
2019-12-06 10:42:59 -0300

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MeuPrimeiroComponent } from './primeiro-contato/meu-primeiro.component';
+import {MeuSegundoComponent} from './meu-segundo/meu-segundo.component'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, 
+                  HelloComponent, 
+                  MeuPrimeiroComponent,
+                  MeuSegundoComponent
+                   ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/meu-segundo/meu-segundo.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'meu-segundo-component',
+  template: `
+  <p style = 'color:red'>Meu segundo component</p>
+  `
+})
+
+export class MeuSegundoComponent{
+
+}
