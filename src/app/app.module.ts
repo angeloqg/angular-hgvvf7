0eda6a406ea3174c4a30aba95be0ca6c244f7eb3
angeloqg <angeloqg@hotmail.com>
2019-12-06 10:53:20 -0300

    3 files modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { MeuPrimeiroComponent } from './primeiro-contato/meu-primeiro.component';
-import {MeuSegundoComponent} from './meu-segundo/meu-segundo.component'
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, 
-                  HelloComponent, 
-                  MeuPrimeiroComponent,
-                  MeuSegundoComponent
-                   ],
+                  HelloComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
