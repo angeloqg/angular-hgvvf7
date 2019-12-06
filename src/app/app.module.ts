2eeb2a6174b7d981f968b691ab23a5b36b8c0848
angeloqg <angeloqg@hotmail.com>
2019-12-06 14:31:05 -0300

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {MenuPrincipal} from './menu-principal/menu-principal.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, 
-                  HelloComponent],
+                  HelloComponent,
+                  MenuPrincipal],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/menu-principal/menu-principal.component.ts
@@ -0,0 +1,20 @@
+import { Component} from '@angular/core';
+
+
+@Component({
+  selector: 'menu-principal',
+  template: `
+    <hr/>
+    <h2>Estudando angular</h2>
+    <p>Estou aprendendo agora</p>
+    <ul>
+      <li>Component</li>
+      <li>Modulos</li>
+      <li>Typescript</li>
+    </ul>
+  `
+})
+
+export class MenuPrincipal{
+
+}
